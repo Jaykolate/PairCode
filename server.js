a4ef0ae03f2ebd161d89f9117ffc7607383e0c7b
jaykolate <jaykolate529@gmail.com>
2026-01-19 18:52:03 +0530

    fix routr

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
   },
 });
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get("/*" ,(req,res) =>{
+app.get(/^(?!\/socket\.io).*/ ,(req,res) =>{
   res.sendFile(path.join(__dirname,'dist','index.html'));
 
 })
